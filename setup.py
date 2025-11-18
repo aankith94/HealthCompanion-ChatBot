ba40586f605e236ec3f2afde715ac3a3bd1b2554
Ankit Kumar <aankith2003@gmail.com>
2025-11-18 18:53:36 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="HealthCompanion-ChatBot",
+    version="0.1.0",
+    author="Ankit Kumar",
+    author_email="aankith2003@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
